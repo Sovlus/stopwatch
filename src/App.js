05b5ecdfc51eab97e161d1c1f11e86df57b7160e
Sovlus <kubbaz122@gmail.com>
2023-04-28 23:03:06 +0200

    function start timer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { useState } from 'react';
 function App() {
   const [time, setTime] = useState(0);
 
+  function startTimer() {
+    setInterval(() => {
+      setTime(prevTime => prevTime +1);
+    }, 1000);
+  }
+
   return (
     <div>
       {}
