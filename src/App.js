e84c7d547a7a28b48eae24e2d3772d9c3fd8b806
Sovlus <kubbaz122@gmail.com>
2023-05-23 00:31:01 +0200

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { useState, useRef } from 'react';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
   const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
@@ -7,7 +7,7 @@ function App() {
 
   function startTimer() {
     intervalRef.current = setInterval(() => {
-      setTime(prevTime => {
+      setTime((prevTime) => {
         const seconds = prevTime.seconds + 1;
         const minutes = prevTime.minutes + Math.floor(seconds / 60);
         const hours = prevTime.hours + Math.floor(minutes / 60);
@@ -27,11 +27,15 @@ function App() {
 
   return (
     <div className='back'>
-      <h1 className="timer">{time.hours.toString().padStart(2, '0')}:{time.minutes.toString().padStart(2, '0')}:{time.seconds.toString().padStart(2, '0')}</h1>
+      <h1 className='timer'>
+        {time.hours.toString().padStart(2, "0")}:
+        {time.minutes.toString().padStart(1, "0")}:
+        {time.seconds.toString().padStart(1, "0")}
+      </h1>
       <div className='btns'>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
-      <button onClick={resetTimer}>Reset</button>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
